2e3c03e2f4375c507519fbe6df6553dfb108b6a5
28nullal <28nullal@derryfield.org>
2025-01-14 18:40:12 -0500

    Update Final_Project.ino

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -12,22 +12,43 @@
  * @param pWire I2c controller
  * @param addr  I2C address(0x64/0x65/0x660x67)
  */
-//DFRobot_HX711_I2C Scale(&Wire,/*addr=*/0x64);
-DFRobot_HX711_I2C Scale;
+//DFRobot_HX711_I2C scale(&Wire,/*addr=*/0x64);
+#include <LiquidCrystal.h>
+#include <DFRobot_HX711_I2C.h>
+#define BUTTON1 7
+DFRobot_HX711_I2C scale;
+int tim = 250;
+bool prevButton1;
+bool currButton1;
 float weight;
 char currCoin;
-float prevVal
+float prevVal;
 float currVal;
-int tim = 250;
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+
+void setup() {
+  scale.begin();
+  pinMode(BUTTON1, INPUT_PULLUP);
+  lcd.begin(16, 2);
+  prevButton1 = false;
+  Serial.begin(9600);
+  while (!scale.begin()) {
+    initPrint1();
+    initPrint2();
+    delay(1000);
+  }
+  scale.setCalibration(2236.f);
+  scale.peel();
+}
 
 char clearArray[] = " Value Cleared! ";
 int clearPosCount;
 void clearPrint() {
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   for (clearPosCount = 0; clearPosCount < 15; clearPosCount++) {
-     lcd.scrollDisplayLeft();
-     lcd.print(clearArray[clearPosCount]);
-     delay(tim);
+    lcd.scrollDisplayLeft();
+    lcd.print(clearArray[clearPosCount]);
+    delay(tim);
   }
   delay(2000);
   lcd.clear();
@@ -36,24 +57,24 @@ void clearPrint() {
 char initArray1[] = "The initialization of the chip failed, please";
 int initPosCount1;
 void initPrint1() {
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   for (initPosCount1 = 0; initPosCount1 < 44; initPosCount1++) {
-     lcd.scrollDisplayLeft();
-     lcd.print(initArray1[initPosCount1]);
-     delay(tim);
+    lcd.scrollDisplayLeft();
+    lcd.print(initArray1[initPosCount1]);
+    delay(tim);
   }
   delay(2000);
   lcd.clear();
 }
 
-char initArray2[] = "confirm whether the chip connection is correct."
-int initPosCount2; 
+char initArray2[] = "confirm whether the chip connection is correct.";
+int initPosCount2;
 void initPrint2() {
-  lcd.setCursor(15,1);
+  lcd.setCursor(15, 1);
   for (initPosCount2 = 0; initPosCount2 < 46; initPosCount2++) {
-     lcd.scrollDisplayLeft();
-     lcd.print(initArray2[initPosCount2]);
-     delay(tim);
+    lcd.scrollDisplayLeft();
+    lcd.print(initArray2[initPosCount2]);
+    delay(tim);
   }
   delay(2000);
   lcd.clear();
@@ -62,7 +83,7 @@ void initPrint2() {
 char weirdArray[] = "Weird / No Coin ";
 int weirdPosCount;
 void weirdPrint() {
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   for (weirdPosCount = 0; weirdPosCount < 16; weirdPosCount++) {
     lcd.scrollDisplayLeft();
     lcd.print(weirdArray[weirdPosCount]);
@@ -75,7 +96,7 @@ void weirdPrint() {
 char scaleArray[] = "";
 int scalePosCount;
 void scalePrint() {
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   for (scalePosCount = 0; scalePosCount < 16; scalePosCount++) {
     lcd.scrollDisplayLeft();
     lcd.print(scaleArray[scalePosCount]);
@@ -97,23 +118,14 @@ int dimePosCount;
 char valQuarter[] = "";
 int quarterPosCount;
 
-
-
-#include <LiquidCrystal.h>
-#include <DFRobot_HX711_I2C.h>
-#define BUTTON1 7
-
-bool prevButton1;
-bool currButton1;
-
 // in INPUT_PULLUP mode, if the signal is HIGH
 // then the button is unpressed.
 bool isPressed(int pin) {
- if (digitalRead(pin) == LOW) {
-   return true;
- } else {
-   return false;
- }
+  if (digitalRead(pin) == LOW) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // this method checks the system to see
@@ -122,36 +134,21 @@ void checkState() {
   // check button 1.
   currButton1 = isPressed(BUTTON1);
   if (currButton1 == true && prevButton1 == false) {
-    Scale.tare();
+    delay(tim);
+    scale.peel();
     prevVal = currVal;
     currVal = 0;
     clearPrint();
     prevButton1 = currButton1;
 
-   } else if (currButton1 == false && currButton1 != prevButton1) {
+  } else if (currButton1 == false && currButton1 != prevButton1) {
     prevButton1 = currButton1;
   }
-  LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
-}
-
-
-void setup() {
-  pinMode(BUTTON1, INPUT_PULLUP);
-  lcd.begin(16, 2);
-  prevButton1 = false;
-  Serial.begin(9600);
-  while (!Scale.begin()) {
-    initPrint1();
-    initPrint2();
-    delay(1000);
-  }
-  Scale.setCalibration(2236.f);
-  Scale.peel();
 }
 
 void loop() {
   checkState();
-  weight = Scale.readWeight();
+  weight = scale.readWeight();
   if (weight >= 2.4 && weight <= 2.6) {
     currCoin = "penny";
   } else if (weight >= 4.9 && weight <= 5.1) {
